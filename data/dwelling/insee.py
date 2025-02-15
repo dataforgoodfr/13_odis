fc423bbfc2264ccb4da166e11a3743600b2ff789
antoningar <antoningaranto@protonmail.com>
2025-02-15 19:50:42 +0100

    feat(logement insee): get logements par type

--- data/dwelling/insee.py
+++ data/dwelling/insee.py
@@ -3,25 +3,6 @@ import requests
 BASE_URL = "https://api.insee.fr/melodi/data/DS_RP_LOGEMENT_PRINC"
 MAX_RESULT = 10000
 
-def get_vacant_dwelling_by_year(year):
-    if not year:
-        assert False, "year must be select"
-    
-    url = f"{BASE_URL}?TIME_PERIOD={year}&OCS=DW_VAC&maxResult={MAX_RESULT}"
-    return get_observations(url)
-
-def get_dwelling_by_year_and_room_count(year, room_count):
-    room_count_labels = ["R1","R2","R3","R4","R_GE5"]
-    if not year:
-        assert False, "year must be select"
-    if not room_count:
-        assert False, "room_count must be select"
-    if room_count not in room_count_labels:
-        assert False, f"room_count must be in {room_count_labels}"
-    
-    url = f"{BASE_URL}?TIME_PERIOD={year}&NOR={room_count}&maxResult={MAX_RESULT}"
-    return get_observations(url)
-
 def get_observations(url):
     observations = []
     isLast = False
@@ -44,3 +25,34 @@ def get_observations(url):
             url = data["paging"]["next"]
 
     return observations
+
+def get_vacant_dwelling_by_year(year):
+    if not year:
+        assert False, "year must be select"
+    
+    url = f"{BASE_URL}?TIME_PERIOD={year}&OCS=DW_VAC&maxResult={MAX_RESULT}"
+    return get_observations(url)
+
+def get_dwelling_by_year_and_room_count(year, room_count):
+    room_count_labels = ["R1","R2","R3","R4","R_GE5"]
+    if not year:
+        assert False, "year must be select"
+    if not room_count:
+        assert False, "room_count must be select"
+    if room_count not in room_count_labels:
+        assert False, f"room_count must be in {room_count_labels}"
+    
+    url = f"{BASE_URL}?TIME_PERIOD={year}&NOR={room_count}&maxResult={MAX_RESULT}"
+    return get_observations(url)
+
+def get_dwelling_by_year_and_type(year, type):
+    types_labels = ["1","2","3T6"]
+    if not year:
+        assert False, "year must be select"
+    if not type:
+        assert False, "type must be select"
+    if type not in types_labels:
+        assert False, f"type must be in {types_labels}"
+    
+    url = f"{BASE_URL}?TIME_PERIOD={year}&TDW={type}&maxResult={MAX_RESULT}"
+    return get_observations(url)
