efc2935e62e3a5f59db59793c09c15d289896046
antoningar <antoningaranto@protonmail.com>
2025-02-15 19:37:18 +0100

    feat(logement insee): get logements vacants

---
+++ data/dwelling/insee.py
@@ -0,0 +1,36 @@
+import requests
+
+BASE_URL = "https://api.insee.fr/melodi/data/DS_RP_LOGEMENT_PRINC"
+MAX_RESULT = 10000
+
+def get_vacant_dwelling_by_year(year):
+    if not year:
+        assert False, "year must be select"
+    
+    url = f"{BASE_URL}?TIME_PERIOD={year}&OCS=DW_VAC&maxResult={MAX_RESULT}"
+    return get_observations(url)
+
+def get_observations(url):
+    observations = []
+    isLast = False
+    url = f"{url}"
+
+    while not isLast:
+        print(url)
+        response = requests.get(url)
+
+        if response.ok:
+            data = response.json()
+            for observation in data["observations"]:
+                observations.append({
+                    **observation["dimensions"],
+                    "value": observation["measures"]["OBS_VALUE_NIVEAU"]["value"]
+                })
+
+        isLast = data["paging"]["isLast"]
+        if "next" in data["paging"]:
+            url = data["paging"]["next"]
+
+    return observations
+
+observations = get_vacant_dwelling_by_year(2021)
