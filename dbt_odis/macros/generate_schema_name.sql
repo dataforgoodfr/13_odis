75618c979307cd278dda8b385c8e7035ec8e4cf3
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2025-02-28 10:43:01 +0100

    correction schema

--- dbt_odis/macros/generate_schema_name.sql
+++ dbt_odis/macros/generate_schema_name.sql
@@ -1,9 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none or custom_schema_name[:-4] == 'live' -%}
+    {%- if custom_schema_name is not none or 'live' in default_schema -%}
 
-        {{ default_schema }}
+        {{ custom_schema_name }}
 
     {%- else -%}
 
