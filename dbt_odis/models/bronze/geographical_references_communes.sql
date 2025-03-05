041e3ed840145216f9ef7d91250feb18b5b14d20
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2025-03-05 19:07:10 +0100

    nouveaux models geocodes et typologies communes

--- dbt_odis/models/bronze/geographical_references_communes.sql
+++ dbt_odis/models/bronze/geographical_references_communes.sql
@@ -8,13 +8,16 @@ with communes as
 (
     select 
         id as id, 
-        json_value(data, '$.uri') as uri, 
+        json_value(data, '$.nom') as nom, 
         json_value(data, '$.code') as code, 
-        json_value(data, '$.type') as type,  
-        json_value(data, '$.intitule') as intitule, 
-        json_value(data, '$.typeArticle') as type_article, 
-        json_value(data, '$.dateCreation') as date_creation,
-        json_value(data, '$.intituleSansArticle') as intitule_sans_article,
+        json_value(data, '$.centre.type') as geo_type,
+        json_value(data, '$.centre.coordinates[0]') as geo_coordonnees_longitude,
+        json_value(data, '$.centre.coordinates[1]') as geo_coordonnees_lattitude,
+        json_value(data, '$.region.nom') as region_nom,
+        json_value(data, '$.region.code') as region_code,
+        json_value(data, '$.departement.nom') as departement_nom,
+        json_value(data, '$.departement.code') as departement_code,
+        json_value(data, '$.population') as population,        
         created_at as created_at
     from {{ source('bronze', 'geographical_references_communes') }} 
 )
--- dbt_odis/models/silver/com_dep_reg.sql
+++ dbt_odis/models/silver/com_dep_reg.sql
@@ -3,37 +3,9 @@
     )
 }}
 
-with communes as 
-(
-    select 
-        code as codgeo, 
-        intitule as libgeo
-    from {{ ref('geographical_references_communes') }}  
-),
-
-departements as 
-(
-    select 
-        code as coddep
-    from {{ ref('geographical_references_departements') }} 
-),
-
-regions as 
-(
-    select 
-        code as codreg
-    from {{ ref('geographical_references_regions') }} 
-)
-
 select 
-    com.codgeo,
-    reg.codreg,
-    dep.coddep,
-    com.libgeo
-
-from communes com 
-    left join regions reg
-        on reg.codreg = substring(com.codgeo, 1, 2)
-    left join departements dep 
-        on dep.coddep = substring(com.codgeo, 1, 2)
-
+    code as CODGEO,
+    region_code as CODREG,
+    departement_code as CODDEP,
+    nom as LIBGEO
+from {{ ref('geographical_references_communes') }} 
