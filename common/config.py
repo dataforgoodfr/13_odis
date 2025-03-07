fa56b3d07d591b615cb2da42b7465c58a642e76e
Guillaume (H&T) <guillaume.patin@gmail.com>
2025-03-07 17:36:07 +0100

    Modélisation objet de la configuration des data sources

--- common/config.py
+++ common/config.py
@@ -1,6 +1,42 @@
 import yaml
+from pydantic_core import ErrorDetails, ValidationError
 
-def load_config(file_path):
-    with open(file_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+from .data_source_model import DataSourceModel
+
+
+def _extract_err_info(e: ErrorDetails) -> str:
+    if len(e["loc"]) > 0:
+        return f"'{e['loc'][0]}' -> {e['msg']} , value passed: '{str(e['input'])}'"
+    return e["msg"]
+
+
+def load_config(file_path: str) -> dict:
+    """the dict representation of the yaml is validated"""
+
+    with open(file_path, "r") as file:
+
+        try:
+
+            yaml_config = yaml.safe_load(file)
+
+            return DataSourceModel(**yaml_config).model_dump()
+
+        except (yaml.YAMLError, TypeError) as e:
+            raise Exception(f"Error parsing YAML file: {str(e)}")
+
+        except ValidationError as v:
+            raise Exception(
+                f"""
+                Error validating YAML file, the structure is not correct,
+                please refer to the documentation for the correct structure. 
+                Following errors were found: 
+                {
+                    ','.join(
+                        map(
+                            _extract_err_info,
+                            v.errors(include_context=True, include_url=True),
+                        )
+                    ) 
+                }
+                """
+            )
---
+++ common/data_source_model.py
@@ -0,0 +1,72 @@
+from typing import Annotated, Literal, Optional, Self
+
+from pydantic import BaseModel, ConfigDict, HttpUrl, StringConstraints, model_validator
+
+EndPoint = Annotated[
+    str,
+    StringConstraints(
+        strip_whitespace=True,
+        pattern=r"^\/.+",  # must start with a '/'
+        min_length=1,
+    ),
+]
+
+
+class HeaderModel(BaseModel):
+
+    accept: Literal["application/json", "application/xml"] = "application/json"
+
+
+class APIModel(BaseModel):
+    """the API section of the yaml file"""
+
+    model_config = ConfigDict(extra="ignore")
+
+    name: str
+    base_url: HttpUrl
+    apidoc: Optional[HttpUrl] = None
+    description: Optional[str] = None
+    default_headers: Optional[HeaderModel] = None
+
+
+class DomainParamsModel(BaseModel):
+    """domain parameters"""
+
+    withColumnName: bool = False
+    withColumnDescription: bool = False
+    withColumnUnit: bool = False
+    page: Optional[int] = 1
+    pageSize: Optional[Literal["all"]] = "all"
+
+
+class DomainModel(BaseModel):
+    """the domain section of the yaml file"""
+
+    API: str
+    type: str
+    endpoint: EndPoint
+    description: Optional[str] = None
+    params: Optional[DomainParamsModel] = None
+    format: Optional[Literal["csv", "json"]] = "json"
+
+
+class DataSourceModel(BaseModel):
+    """global model for the yaml file"""
+
+    APIs: dict[str, APIModel]
+    domains: dict[str, dict[str, DomainModel]]
+
+    # when loading the domains, we need to check that the API is in the APIs dict
+    # this is done by the validator
+    @model_validator(mode="after")
+    def check_domain_api(self) -> Self:
+        """
+        verify the domain API is in the APIs dict
+        """
+        for domain in self.domains.values():
+            for subdomain in domain.values():
+                if subdomain.API not in self.APIs:
+                    raise ValueError(f"API '{subdomain.API}' not found in APIs section")
+
+        return self
+        return self
