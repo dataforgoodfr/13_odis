9d6abb6a1b3dcd8afe3739d999c405e9f3f5b20e
gustelle <guillaume.patin@gmail.com>
2025-03-15 20:27:06 +0100

    simplify code

--- bin/odis.py
+++ bin/odis.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
 import argparse
+
 import sys
 import os
 from pprint import PrettyPrinter
+
+import datetime
+
 import json
+import os
+import sys
 from importlib import import_module
+from io import StringIO
+from pprint import PrettyPrinter
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from common.config import load_config
-from common.utils.logging_odis import logger
+from common.data_source_model import DataSourceModel
 from common.utils.file_handler import FileHandler
+
 from common.data_source_model import DataProcessLog
+
+from common.utils.logging_odis import logger
+
 
 pp = PrettyPrinter(indent=4)
 fh = FileHandler()
+
 
 # define globals
 DEFAULT_CONFIGFILE = 'datasources.yaml'
@@ -51,6 +64,19 @@ def explain(apis:list[str] = None, domain:str = None, sources:list[str] = None, 
     apis_list = list(all_apis.keys())
 
     all_domains = CONFIG.get('domains')
+
+
+def explain_source(
+    config, apis=None, domain=None, models=None, default_all=False
+) -> str:
+
+    output = StringIO()
+
+    all_apis = config.get("APIs")
+    apis_list = list(all_apis.keys())
+
+    all_domains = config.get("domains")
+
     domains_list = list(all_domains.keys())
 
     models_list = []
@@ -63,48 +89,94 @@ def explain(apis:list[str] = None, domain:str = None, sources:list[str] = None, 
             all_models.append(full_model_info)
 
     # Pretty print information about the APIs
-    print("\n\n================= API explanations =================\n")
-    print(f"Available APIs from config : {apis_list}")
-    api_selection = [(api,all_apis.get(api)) for api in apis] if apis else []
-    for api_name,api_conf in api_selection:
-        print(f"\n~~~~~~ {api_name} ~~~~~~")
-        print(f"{json.dumps(api_conf,indent=4)}")
+    output.write("\n\n================= API explanations =================\n")
+    output.write(f"Available APIs from config : {apis_list}")
+    api_selection = [(api, all_apis.get(api)) for api in apis] if apis else []
+    for api_name, api_conf in api_selection:
+        output.write(f"\n~~~~~~ {api_name} ~~~~~~")
+        output.write(f"{json.dumps(api_conf,indent=4)}")
         used_in = [
-            (model[0],model[1]) for model in all_models if model[2]['API'] == api_name 
+            (model[0], model[1]) for model in all_models if model[2]["API"] == api_name
         ]
-        print(f"This API is used in the following models : {pp.pformat(used_in)}\n")
-
+        output.write(
+            f"This API is used in the following models : {pp.pformat(used_in)}\n"
+        )
 
     # Pretty print information about the domains and source models
-    print("\n\n================= Domain & Models explanations =================\n")
-    print(f"Available Domains from config : {domains_list}\n")
-    print(f"Available Models from config : {models_list}\n")
+    output.write(
+        "\n\n================= Domain & Models explanations =================\n"
+    )
+    output.write(f"Available Domains from config : {domains_list}\n")
+    output.write(f"Available Models from config : {models_list}\n")
 
     models_to_explain = []
+
     
     if sources:
         models_to_explain = [model for model in all_models if model[1] in sources]
+
+
+    if models:
+        models_to_explain = [model for model in all_models if model[1] in models]
+
     else:
         if domain:
             domain_to_explain = all_domains.get(domain)
-            models_to_explain = [model for model in all_models if model[1] in domain_to_explain.keys()]
-            print("------------------------------------------------")
-            print(f"Current selected domain: {domain}")
-            print("------------------------------------------------\n")
+            models_to_explain = [
+                model for model in all_models if model[1] in domain_to_explain.keys()
+            ]
+            output.write("------------------------------------------------")
+            output.write(f"Current selected domain: {domain}")
+            output.write("------------------------------------------------\n")
         else:
             models_to_explain = []
 
     for domain_name, model_name, model_conf in models_to_explain:
-        print(f"\nSOURCE MODEL: {domain_name} :: {model_name}")
-        print(f"{json.dumps(model_conf,indent=4)}")
+        output.write(f"\nSOURCE MODEL: {domain_name} :: {model_name}")
+        output.write(f"{json.dumps(model_conf,indent=4)}")
 
-def extract(domain:str = None, sources:list[str] = None, **params):
+    return output.getvalue()
+
+
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description="Extract data from configured sources")
+    parser.add_argument("-d", "--domain", help="Extract data for specific domain only")
+    parser.add_argument(
+        "-s", "--sources", help="List of sources to extract from", nargs="*"
+    )
+    parser.add_argument(
+        "--config", default="datasources.yaml", help="Path to datasources config file"
+    )
+    parser.add_argument(
+        "-e", "--explain", help="Explain the chosen data sources", action="store_true"
+    )
+    parser.add_argument("--apis", help="APIs to be explained", nargs="*")
+
+    return parser.parse_args()
+
+
+def extract_data(config, domain=None, sources=None):
+
+    if config["domains"][domain]:
+        all_sources = config["domains"][domain].keys()
+        if not all_sources:
+            print(f"No sources found for domain: {domain}")
+            return
+
 
     # Set the source list : if none was given in input, take all for given domain
     sources_list = sources if sources else SOURCES_INDEX[domain]
 
+
     # start_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     
+
+    start_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+
     for source_name in sources_list:
         
         # initialize process log
@@ -112,27 +184,46 @@ def extract(domain:str = None, sources:list[str] = None, **params):
         
         print(f"Extracting source: {domain}/{source_name}")
 
+
         source = DOMAINS[domain][source_name]
         
         source_type = source['type']
+
+        source = config["domains"][domain][source_name]
+
+        source_type = source["type"]
+
         if not source_type:
             logger.info(f"Source type not specified for {source_name}, skipping...")
             continue
-        
+
         # Import the Extractor class specified in the source config
-        source_type = source.get('type')
-        extractor_class = get_connector_class(source_type, 'source_extractors')
-            
+        source_type = source.get("type")
+        extractor_class = get_connector_class(source_type, "source_extractors")
+
         try:
             # Instantiate the Extractor and execute download
+
             extractor = extractor_class(CONFIG,domain)
 
             # All extractors 'download' must yield iterable results
             extract_generator = extractor.download(domain, source_name)
     
+
+            extractor = extractor_class(config, domain)
+
+            # All extractors 'download' must yield iterable results
+            extract_generator = extractor.download(domain, source_name)
+
+            file_dumps = []
+            last_page_downloaded = 0
+            complete = False
+
+
             # iterate through the extraction generator
             for _, page_number, is_last, filepath in extract_generator:
                 processlog.add_pagelog(page_number, filepath = filepath, is_last = is_last)
+
 
             # save the process log locally
             fh.file_dump(domain, f"{source_name}_extract_log", payload = processlog.to_dict())
@@ -145,6 +236,47 @@ def load(domain:str = None, sources:list[str] = None, **params):
     
     # Set the source list : if none was given in input, take all for given domain
     sources_list = sources if sources else SOURCES_INDEX[domain]
+
+                last_page_downloaded = page_number
+                complete = is_last
+                filedump_info = {"page": page_number, "filepath": filepath}
+                file_dumps.append(filedump_info)
+
+            logger.debug(f"LAST PAGE DOWNLOADED: {last_page_downloaded}")
+
+            # dump the log of the full extract iteration
+            extract_metadata = {
+                "domain": domain,
+                "source": source_name,
+                "last_run_time": start_time,
+                "last_page_downloaded": last_page_downloaded,
+                "successfully_completed": complete,
+                "file_dumps": file_dumps,
+            }
+            fh.file_dump(domain, f"{source_name}_metadata", payload=extract_metadata)
+
+        except Exception as e:
+            logger.exception(f"Error extracting data from {source_name}: {str(e)}")
+
+
+def main():
+
+    args = parse_args()
+    needs_explanation = args.explain
+
+    try:
+        config = load_config(args.config)
+
+    except Exception as e:
+        logger.info(f"Error loading config file: {str(e)}")
+        sys.exit(1)
+
+    if needs_explanation:
+        explanations = explain_source(
+            config, domain=args.domain, apis=args.apis, models=args.sources
+        )
+        print(explanations)
+
 
     for source_name in sources_list:
 
@@ -166,6 +298,7 @@ def load(domain:str = None, sources:list[str] = None, **params):
         
         except Exception as e:
             logger.exception(f"Issue in loading data : {e}")
+
 
 if __name__ == '__main__':
     
@@ -191,3 +324,8 @@ if __name__ == '__main__':
     
     # execute operation with args
     operation(**vars(args))
+
+
+if __name__ == "__main__":
+    main()
+
--- common/config.py
+++ common/config.py
@@ -1,29 +1,32 @@
+from typing import Type
+
 import yaml
-from pydantic_core import ValidationError
-
-from .data_source_model import DataSourceModel
 
 
-def load_config(file_path: str, strict: bool = True) -> dict:
+def load_config[T](file_path: str, response_model: Type[T] = dict) -> T:
     """
-    the dict representation of the yaml is validated
+    the representation of the yaml is validated against the `response_model` provided,
+    and the yaml is loaded into a python object of type `response_model`
 
     Args:
         file_path (str): the path to the yaml file
-        strict (bool): if True, the yaml file must match a `DataSourceModel` schema, defaults to True
+        response_model (Any): the model to validate the yaml against. Defaults to dict.
+
+    Returns:
+        Any: the python object representation of the yaml file
+
+    Example:
+    ```python
+    from common.config import load_config
+    from common.data_source_model import DataSourceModel
+
+    config = load_config("path/to/config.yml", response_model=DataSourceModel)
+    ```
 
     """
 
     with open(file_path, "r") as file:
 
-        try:
+        yaml_config = yaml.safe_load(file)
 
-            yaml_config = yaml.safe_load(file)
-
-            if strict:
-                return DataSourceModel(**yaml_config).model_dump(mode="json")
-
-            return yaml_config
-
-        except (yaml.YAMLError, TypeError, ValidationError):
-            raise
+        return response_model(**yaml_config)
