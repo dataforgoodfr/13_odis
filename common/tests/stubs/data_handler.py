65035c2de69ab313f178655027b50cfe9c8deb3b
gustelle <guillaume.patin@gmail.com>
2025-03-22 12:27:44 +0100

    test: added tests

---
+++ common/tests/stubs/data_handler.py
@@ -0,0 +1,14 @@
+from common.utils.interfaces.data_handler import IDataHandler, StorageInfo
+
+
+class StubDataHandler(IDataHandler):
+    """used to test the JsonExtractor class"""
+
+    is_handled: bool = False
+
+    def handle(self, *args, **kwargs) -> StorageInfo:
+
+        self.is_handled = True
+        return StorageInfo(
+            location="test", format="test", file_name="test", encoding="test"
+        )
