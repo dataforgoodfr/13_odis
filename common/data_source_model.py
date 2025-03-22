7779e87d3e97c7934e5b93a0f70a8a59592044e8
antoningar <antoningaranto@protonmail.com>
2025-03-22 18:39:21 +0100

    feat(notebook extractor): set up

--- common/data_source_model.py
+++ common/data_source_model.py
@@ -65,14 +65,15 @@ class DomainModel(BaseModel):
 
     """
 
-    API: str
+    API: str = ""
     type: str
-    endpoint: EndPoint
+    endpoint: EndPoint = ""
     description: Optional[str] = None
     headers: Optional[HeaderModel] = Field(
         default_factory=HeaderModel,
         description="headers to be sent with the request",
     )
+    filename: str = ""
 
     extract_params: Optional[dict] = Field(
         default=None,
@@ -142,7 +143,7 @@ class ConfigurationModel(BaseModel):
 class DataSourceModel(ConfigurationModel):
     """global model for the yaml file"""
 
-    APIs: dict[str, APIModel]
+    APIs: dict[str, APIModel] = {}
     domains: dict[str, dict[str, DomainModel]]
 
     # when loading the domains, we need to check that the API is in the APIs dict
@@ -152,10 +153,10 @@ class DataSourceModel(ConfigurationModel):
         """
         verify the domain API is in the APIs dict
         """
-        for domain in self.domains.values():
-            for model in domain.values():
-                if model.API not in self.APIs:
-                    raise ValueError(f"API '{model.API}' not found in APIs section")
+        # for domain in self.domains.values():
+        #     for model in domain.values():
+        #         if model.API not in self.APIs:
+        #             raise ValueError(f"API '{model.API}' not found in APIs section")
 
         return self
 
@@ -166,8 +167,8 @@ class DataSourceModel(ConfigurationModel):
         """
         for domain in self.domains.values():
             for model in domain.values():
-                api = self.APIs[model.API]
-                model.merge_headers(api.default_headers)
+                api = self.APIs.get(model.API, None)
+                model.merge_headers(api.default_headers) if api is not None else None
 
         return self
 
@@ -317,4 +318,94 @@ class DataSourceModel(ConfigurationModel):
             if model.API == key:
                 return api
 
+<<<<<<< HEAD
         raise ValueError(f"API '{model.API}' not found in APIs section")
+=======
+        # raise ValueError(f"API '{model.API}' not found in APIs section")
+
+
+@dataclass
+class PageLog:
+    """model for easily updating and logging information about the processing of a given page"""
+
+    pageno: int
+    filepath: Optional[str] = None  # to be enhanced ; needs to be a valid path
+    is_last: Optional[bool] = False
+    extracted: Optional[bool] = False
+    bronze_loaded: Optional[bool] = False
+
+
+@dataclass
+class DataProcessLog:
+    """model for exchanging processing reports between extractors and loaders"""
+
+    domain: str
+    source: str
+    operation: str
+    last_run_time: str = field(
+        default_factory=lambda: datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+    )
+    exctracted_pages: int = 0
+    loaded_pages: int = 0
+    successfully_completed: bool = False
+    source_config: dict = None
+    pages: dict[PageLog] = field(default_factory=dict)
+
+    @classmethod
+    def from_dict(cls, dict_data):
+
+        processed_dict_data = dict_data
+
+        # Pop out dict-type pagelogs to reinsert PageLog types
+        pagelogs = {}
+        pages_dict = processed_dict_data.pop("pages")
+        for pageno, pagelog_dict in pages_dict.items():
+            pagelog = PageLog(**pagelog_dict)
+            pagelogs[pageno] = pagelog
+
+        processed_dict_data["pages"] = pagelogs
+
+        return cls(**processed_dict_data)
+
+    def to_dict(self):
+
+        raw_dict = self.__dict__
+        serialized_pagelogs = {}
+        for pageno, pagelog in self.pages.items():
+            serialized_pagelogs[pageno] = pagelog.__dict__
+
+        # replace the PageLog list by serializable list(dict)
+        raw_dict.pop("pages")
+        serialized_dict = raw_dict
+        serialized_dict["pages"] = serialized_pagelogs
+
+        return serialized_dict
+
+    def update_pagelog(self, pageno: int, **pagelog_params):
+        """Creates or Updates a PageLog with the information of how its last processing (extract or load) went"""
+
+        pagelog = self.pages.get(str(pageno))
+
+        if pagelog is None:
+            # if pagenumber was not found, create a new PageLog
+            pagelog = PageLog(pageno, **pagelog_params)
+
+        else:
+            # Update PageLog in place
+            for key, value in pagelog_params.items():
+                setattr(pagelog, key, value)
+
+        # Add page log to pages
+        self.pages[pageno] = pagelog
+
+        # If relevant, increment the extracted pages count
+        if self.operation == "extract" and pagelog.extracted:
+            self.exctracted_pages += 1
+
+        # If relevant, increment the loaded pages count
+        if self.operation == "load" and pagelog.bronze_loaded:
+            self.loaded_pages += 1
+
+        # If this page was the last, then infer process is successful and complete
+        self.successfully_completed = pagelog.is_last
+>>>>>>> f54922e (feat(notebook extractor): set up)
