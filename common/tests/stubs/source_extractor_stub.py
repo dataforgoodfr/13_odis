ab97f475e39bf9615fd4e7ad2c0b0bde5f699f9e
gustelle <guillaume.patin@gmail.com>
2025-03-22 12:27:44 +0100

    test: added tests

--- common/tests/stubs/source_extractor_stub.py
+++ common/tests/stubs/source_extractor_stub.py
@@ -1,13 +1,15 @@
 from typing import Generator
 
-from common.utils.interfaces.extractor import ExtractionResult
-from common.utils.source_extractors import JsonExtractor
+from common.utils.interfaces.extractor import AbstractSourceExtractor, ExtractionResult
 
 
-class StubExtractor(JsonExtractor):
+class StubExtractor(AbstractSourceExtractor):
     """used to test the JsonExtractor class"""
 
     is_download: bool = False
+
+    def __init__(self, config, model, handler, metadata_handler):
+        super().__init__(config, model, handler, metadata_handler)
 
     def download(self) -> Generator[ExtractionResult]:
 
@@ -15,3 +17,24 @@ class StubExtractor(JsonExtractor):
         yield ExtractionResult(
             payload={"test": "test"}, page_number=1, is_last=True, filepath="test"
         )
+
+
+class StubIterationExtractor(AbstractSourceExtractor):
+    """a stub extractor that iterates 10 times"""
+
+    is_download: bool = False
+    iteration_count: int = 0
+    expected_iterations: int = 10
+
+    def __init__(self, config, model, handler, metadata_handler):
+        super().__init__(config, model, handler, metadata_handler)
+
+    def download(self) -> Generator[ExtractionResult]:
+
+        self.is_download = True
+
+        for _i in range(1, self.expected_iterations):
+            self.iteration_count += 1
+            yield ExtractionResult(
+                payload={"test": "test"}, page_number=1, is_last=True, filepath="test"
+            )
