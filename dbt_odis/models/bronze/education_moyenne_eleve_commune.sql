9d934bf661480899562220198e0dd9146ca013df
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2025-04-18 19:35:43 +0200

    update variabilser le bronze demandeur emploi + tests and fixes persist_docs

--- dbt_odis/models/bronze/education_moyenne_eleve_commune.sql
+++ dbt_odis/models/bronze/education_moyenne_eleve_commune.sql
@@ -8,11 +8,14 @@ with eleve_commune as
 (
     select 
         id as id, 
-        json_value(data, '$.commune') as commune, 
-        json_value(data, '$.moyenne_eleves_par_classe') as moyenne_eleves_par_classe, 
+        {{ generate_flatten_json(
+            model_name = {{ source('bronze', 'education_moyenne_eleve_commune') }},
+            json_column = 'data'
+        ) }}
         created_at as created_at
 
-    from {{ source('bronze', 'education_moyenne_eleve_commune') }}  
+    from  {{ source('bronze', 'education_moyenne_eleve_commune') }}
 )
 
 select * from eleve_commune
+
