33448291db3d5e44d63a001d8d2418b7d6309d3c
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2025-04-24 19:52:51 +0200

    update écriture + bmo

--- dbt_odis/models/bronze/education_moyenne_eleve_commune.sql
+++ dbt_odis/models/bronze/education_moyenne_eleve_commune.sql
@@ -7,12 +7,14 @@
 with eleve_commune as 
 (
     select 
-        id as id, 
-        {{ generate_flatten_json_education(
+        id, 
+        {# {{ generate_flatten_json_education(
             model_name = source('bronze', 'education_moyenne_eleve_commune'),
             json_column = 'data'
-        ) }},
-        created_at as created_at
+        ) }}, #}
+        (data::jsonb) ->> 'commune' as commune, 
+        (data::jsonb) ->> 'moyenne_eleves_par_classe' as moyenne_eleves_par_classe, 
+        created_at
 
     from  {{ source('bronze', 'education_moyenne_eleve_commune') }}
 )
--- dbt_odis/models/bronze/education_moyenne_eleve_region.sql
+++ dbt_odis/models/bronze/education_moyenne_eleve_region.sql
@@ -7,10 +7,10 @@
 with eleve_region as 
 (
     select 
-        id as id, 
-        json_value(data, '$.region_academique') as region_academique, 
-        json_value(data, '$.moyenne_eleves_par_classe') as moyenne_eleves_par_classe, 
-        created_at as created_at
+        id, 
+        (data::jsonb) ->> 'region' as region, 
+        (data::jsonb) ->> 'moyenne_eleves_par_classe' as moyenne_eleves_par_classe, 
+        created_at
 
     from {{ source('bronze', 'education_moyenne_eleve_region') }}  
 )
