e7be4248d505ea387856d062704854830a274bad
Victorien-M <maillet.victorien@gmail.com>
2025-06-06 16:40:11 +0200

    education_moyenne_eleve_silver_union

--- dbt_odis/models/silver/education_moyenne_eleve.sql
+++ dbt_odis/models/silver/education_moyenne_eleve.sql
@@ -16,7 +16,10 @@ Select * FROM {{ ref('education_moyenne_eleve_region') }} AS D
 
 
 
-WITH geo_communes AS (
+
+  {#   code final #}
+
+  WITH geo_communes AS (
     SELECT 
         code,
         UPPER(TRANSLATE(nom, 'àâäáãåçéèêëíìîïñóòôöõúùûüýÿ', 'aaaaaaceeeeiiiinooooouuuuyy')) AS commune
@@ -44,8 +47,61 @@ education_communes AS (
         nombre_eleves_preelementaire_hors_ulis,
         created_at
     FROM {{ ref('education_moyenne_eleve_commune') }}
-)
+),
 
+geo_departement AS (
+    SELECT 
+        code,
+        UPPER(TRANSLATE(intitule, 'àâäáãåçéèêëíìîïñóòôöõúùûüýÿ', 'aaaaaaceeeeiiiinooooouuuuyy')) AS departement
+    FROM {{ ref('geographical_references_departements') }}
+),
+
+education_dep AS (
+    SELECT 
+        id,
+        departement,
+        academie,
+        region_academique,
+        rentree_scolaire,
+        nombre_total_eleves,
+        nombre_total_classes,
+        nombre_eleves_ulis,
+        nombre_eleves_cp_hors_ulis,
+        nombre_eleves_ce1_hors_ulis,
+        nombre_eleves_ce2_hors_ulis,
+        nombre_eleves_cm1_hors_ulis,
+        nombre_eleves_cm2_hors_ulis,
+        nombre_eleves_elementaire_hors_ulis,
+        nombre_eleves_preelementaire_hors_ulis,
+        created_at
+    FROM {{ ref('education_moyenne_eleve_departement') }}
+),
+
+geo_regions AS (
+    SELECT 
+        code,
+        UPPER(TRANSLATE(intitule, 'àâäáãåçéèêëíìîïñóòôöõúùûüýÿ', 'aaaaaaceeeeiiiinooooouuuuyy')) AS region
+    FROM {{ ref('geographical_references_regions') }}
+),
+
+education_regions AS (
+    SELECT 
+        id,
+        region_academique,
+        rentree_scolaire,
+        nombre_total_eleves,
+        nombre_total_classes,
+        nombre_eleves_ulis,
+        nombre_eleves_cp_hors_ulis,
+        nombre_eleves_ce1_hors_ulis,
+        nombre_eleves_ce2_hors_ulis,
+        nombre_eleves_cm1_hors_ulis,
+        nombre_eleves_cm2_hors_ulis,
+        nombre_eleves_elementaire_hors_ulis,
+        nombre_eleves_preelementaire_hors_ulis,
+        created_at
+    FROM {{ ref('education_moyenne_eleve_region') }}
+)
 
 
 SELECT 
@@ -67,47 +123,18 @@ SELECT
     e.nombre_eleves_cm2_hors_ulis,
     e.nombre_eleves_elementaire_hors_ulis,
     e.nombre_eleves_preelementaire_hors_ulis,
-    e.created_at    
+    e.created_at
 FROM education_communes AS e
 LEFT JOIN geo_communes AS g
-    ON e.commune = UPPER(TRANSLATE(g.commune, 'àâäáãåçéèêëíìîïñóòôöõúùûüýÿ', 'aaaaaaceeeeiiiinooooouuuuyy'))
+    ON e.commune = g.commune
 
-
-WITH geo_departement AS (
-    SELECT 
-        code,
-        UPPER(TRANSLATE(nom, 'àâäáãåçéèêëíìîïñóòôöõúùûüýÿ', 'aaaaaaceeeeiiiinooooouuuuyy')) AS commune
-    FROM {{ ref('geographical_references_departements') }}
-),
-
-education_dep AS (
-    SELECT 
-        id,
-        NULL AS code_postal,
-        NULL AS commune,
-        departement,
-        academie,
-        region_academique,
-        rentree_scolaire,
-        nombre_total_eleves,
-        nombre_total_classes,
-        nombre_eleves_ulis,
-        nombre_eleves_cp_hors_ulis,
-        nombre_eleves_ce1_hors_ulis,
-        nombre_eleves_ce2_hors_ulis,
-        nombre_eleves_cm1_hors_ulis,
-        nombre_eleves_cm2_hors_ulis,
-        nombre_eleves_elementaire_hors_ulis,
-        nombre_eleves_preelementaire_hors_ulis,
-        created_at
-    FROM {{ ref('education_moyenne_eleve_departement') }}
-)
+UNION ALL
 
 SELECT 
     h.code AS code_insee,
     f.id,
-    f.code_postal,
-    f.commune,
+    NULL AS code_postal,
+    NULL AS commune,
     f.departement,
     f.academie,
     f.region_academique,
@@ -122,146 +149,33 @@ SELECT
     f.nombre_eleves_cm2_hors_ulis,
     f.nombre_eleves_elementaire_hors_ulis,
     f.nombre_eleves_preelementaire_hors_ulis,
-    f.created_at    
+    f.created_at
 FROM education_dep AS f
 LEFT JOIN geo_departement AS h
-    ON f.commune = UPPER(TRANSLATE(h.commune, 'àâäáãåçéèêëíìîïñóòôöõúùûüýÿ', 'aaaaaaceeeeiiiinooooouuuuyy'))
+    ON f.departement = h.departement
 
-
-
-{# #FAIRE UN SELECT + JOINTURE #}
-
-Select * FROM {{ ref('geographical_references_regions') }} 
-Select * FROM {{ ref('education_moyenne_eleve_region') }} AS D
-
-
-
-WITH geo_regions AS (
-    SELECT 
-        code,
-        UPPER(TRANSLATE(nom, 'àâäáãåçéèêëíìîïñóòôöõúùûüýÿ', 'aaaaaaceeeeiiiinooooouuuuyy')) AS commune
-    FROM {{ ref('geographical_references_regions') }}
-),
-
-education_regions AS (
-    SELECT 
-        id,
-        NULL AS code_postal,
-        NULL AS commune,
-        NULL AS departement,
-        NULL AS academie,
-        region_academique,
-        rentree_scolaire,
-        nombre_total_eleves,
-        nombre_total_classes,
-        nombre_eleves_ulis,
-        nombre_eleves_cp_hors_ulis,
-        nombre_eleves_ce1_hors_ulis,
-        nombre_eleves_ce2_hors_ulis,
-        nombre_eleves_cm1_hors_ulis,
-        nombre_eleves_cm2_hors_ulis,
-        nombre_eleves_elementaire_hors_ulis,
-        nombre_eleves_preelementaire_hors_ulis,
-        created_at
-    FROM {{ ref('education_moyenne_eleve_region') }}
-)
-
-
-
-{# #FAIRE UN SELECT + JOINTURE #}
-
-{# PUIS UN UNION ALL DES 3 TABLES #}
-
-union_c_d_r AS (
-    SELECT 
-        id,
-        code_postal,
-        commune,
-        departement,
-        academie,
-        region_academique,
-        rentree_scolaire,
-        nombre_total_eleves,
-        nombre_total_classes,
-        nombre_eleves_ulis,
-        nombre_eleves_cp_hors_ulis,
-        nombre_eleves_ce1_hors_ulis,
-        nombre_eleves_ce2_hors_ulis,
-        nombre_eleves_cm1_hors_ulis,
-        nombre_eleves_cm2_hors_ulis,
-        nombre_eleves_elementaire_hors_ulis,
-        nombre_eleves_preelementaire_hors_ulis,
-        created_at
-    FROM {{ ref('education_moyenne_eleve_commune') }}
-
-    UNION ALL
-
-    SELECT 
-        id,
-        NULL AS code_postal,
-        NULL AS commune,
-        departement,
-        academie,
-        region_academique,
-        rentree_scolaire,
-        nombre_total_eleves,
-        nombre_total_classes,
-        nombre_eleves_ulis,
-        nombre_eleves_cp_hors_ulis,
-        nombre_eleves_ce1_hors_ulis,
-        nombre_eleves_ce2_hors_ulis,
-        nombre_eleves_cm1_hors_ulis,
-        nombre_eleves_cm2_hors_ulis,
-        nombre_eleves_elementaire_hors_ulis,
-        nombre_eleves_preelementaire_hors_ulis,
-        created_at
-    FROM {{ ref('education_moyenne_eleve_departement') }}
-
-    UNION ALL
-
-    SELECT 
-        id,
-        NULL AS code_postal,
-        NULL AS commune,
-        NULL AS departement,
-        NULL AS academie,
-        region_academique,
-        rentree_scolaire,
-        nombre_total_eleves,
-        nombre_total_classes,
-        nombre_eleves_ulis,
-        nombre_eleves_cp_hors_ulis,
-        nombre_eleves_ce1_hors_ulis,
-        nombre_eleves_ce2_hors_ulis,
-        nombre_eleves_cm1_hors_ulis,
-        nombre_eleves_cm2_hors_ulis,
-        nombre_eleves_elementaire_hors_ulis,
-        nombre_eleves_preelementaire_hors_ulis,
-        created_at
-    FROM {{ ref('education_moyenne_eleve_region') }}
-)
+UNION ALL
 
 SELECT 
-    G.code AS code_insee,
-    G.region_code AS region_code,
-    F.id,
-    F.code_postal,
-    F.commune,
-    F.departement,
-    F.academie,
-    F.region_academique,
-    F.rentree_scolaire,
-    F.nombre_total_eleves,
-    F.nombre_total_classes,
-    F.nombre_eleves_ulis,
-    F.nombre_eleves_cp_hors_ulis,
-    F.nombre_eleves_ce1_hors_ulis,
-    F.nombre_eleves_ce2_hors_ulis,
-    F.nombre_eleves_cm1_hors_ulis,
-    F.nombre_eleves_cm2_hors_ulis,
-    F.nombre_eleves_elementaire_hors_ulis,
-    F.nombre_eleves_preelementaire_hors_ulis,
-    F.created_at
-FROM union_c_d_r AS F
-LEFT JOIN geo AS G
-    ON G.commune = UPPER(TRANSLATE(F.commune, 'àâäáãåçéèêëíìîïñóòôöõúùûüýÿ', 'aaaaaaceeeeiiiinooooouuuuyy'))
+    r.code AS code_insee,
+    e.id,
+    NULL AS code_postal,
+    NULL AS commune,
+    NULL AS departement,
+    NULL AS academie,
+    e.region_academique,
+    e.rentree_scolaire,
+    e.nombre_total_eleves,
+    e.nombre_total_classes,
+    e.nombre_eleves_ulis,
+    e.nombre_eleves_cp_hors_ulis,
+    e.nombre_eleves_ce1_hors_ulis,
+    e.nombre_eleves_ce2_hors_ulis,
+    e.nombre_eleves_cm1_hors_ulis,
+    e.nombre_eleves_cm2_hors_ulis,
+    e.nombre_eleves_elementaire_hors_ulis,
+    e.nombre_eleves_preelementaire_hors_ulis,
+    e.created_at
+FROM education_regions AS e
+LEFT JOIN geo_regions AS r
+    ON e.region_academique = r.region
