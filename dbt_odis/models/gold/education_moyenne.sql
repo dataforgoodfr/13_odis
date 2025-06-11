44cb3511720a1448bd065e4b4a9f812d8c9c11e8
Victorien-M <maillet.victorien@gmail.com>
2025-06-11 17:22:23 +0200

    education_moyenne_gold

--- dbt_odis/models/gold/education_moyenne.sql
+++ dbt_odis/models/gold/education_moyenne.sql
@@ -7,6 +7,26 @@
 {# #select les colonnes du silver pour correspondre à la table du drive  #}
 
 
+Select
+*
+from {{ ref('education_moyenne_eleve') }}
+WHERE code_geo = 73
+ 
 
-Select *
-    from {{ ref('education_moyenne_eleve') }}
+
+ 
+
+
+SELECT
+  code_geo AS codgeo,
+  rentree_scolaire AS year,
+  CASE
+    WHEN nombre_total_classes::numeric != 0 THEN ROUND(nombre_total_eleves::numeric / nombre_total_classes::numeric,2)
+    ELSE NULL
+  END AS nombre_moyen_eleves_classe,
+  nombre_total_eleves AS "Nombre total d'élèves",
+  nombre_total_classes AS "Nombre total de classes"
+FROM {{ ref('education_moyenne_eleve') }}
+ORDER BY code_geo, rentree_scolaire ASC;
+
+
