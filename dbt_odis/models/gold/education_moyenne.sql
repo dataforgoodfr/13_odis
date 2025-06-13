58c8ff694a349e16a3e80f8951346a8af8caccf3
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2025-06-14 00:21:08 +0200

    data types et gold correction

--- dbt_odis/models/gold/education_moyenne.sql
+++ dbt_odis/models/gold/education_moyenne.sql
@@ -9,11 +9,11 @@ select
     when type_geo = 'region' then concat('reg',code_geo)
     else code_geo
   end as codgeo,
-  extract(year from cast(rentree_scolaire || '-01-01' as date)) as YEAR,
+  extract(year from cast(rentree_scolaire || '-01-01' as date)) as "YEAR",
   case
     when nombre_total_classes != 0 then round(nombre_total_eleves / nombre_total_classes,2)
   else null
-  end as nombre_moyen_eleves_classe,
+  end as "Nombre_Moyen_Eleves_Classe",
   nombre_total_eleves as "Nombre total d'élèves",
   nombre_total_classes as "Nombre total de classes"
 from {{ ref('education_moyenne_eleve') }}
