6c9a16fbd319cd489c4549e7849da56e0ec13214
Kimberly Black <kimberly.n.black@gmail.com>
2025-06-22 21:37:16 +0000

    feat: logement social bronze to silver
    - Modèles bronze logement social : noms de colonnes en minuscules
    - Ajout de 4 macros :
      • build_historical_indicator_cte
      • build_static_indicator_cte
      • extract_year_logement_logements_sociaux
      • infer_current_year_from_historical_columns
    - Ajout de 5 modèles silver :
      • stg_logement_logements_sociaux_communes
      • stg_logement_logements_sociaux_departement
      • stg_logement_logements_sociaux_region
      • stg_logement_logements_sociaux_epci
      • silver_logements_sociaux
    - Ajout d un modèle gold :
      • gold_logement_logements_sociaux
    - Mise à jour des fichiers yaml silver__models et gold__models

--- dbt_odis/models/bronze/logement_logements_sociaux_region.sql
+++ dbt_odis/models/bronze/logement_logements_sociaux_region.sql
@@ -1,9 +1,14 @@
 {{ config(
     tags = ['bronze', 'logement_social'],
-    alias = 'vw_logement_social_logements_sociaux_region'
+    alias = 'vw_logement_logements_sociaux_region'
     )
 }}
 
-select 
-    {{ dbt_utils.star(from=source('bronze', 'logement_social_logements_sociaux_region')) }}
+select
+    "REG" as reg,
+    "LIBREG" as libreg,
+    {{ dbt_utils.star(
+        from=source('bronze', 'logement_social_logements_sociaux_region'),
+        except=["REG", "LIBREG"] 
+    ) }}
 from {{ source('bronze', 'logement_social_logements_sociaux_region') }}
---
+++ dbt_odis/models/silver/stg_logement_logements_sociaux_departement.sql
@@ -0,0 +1,125 @@
+{{ config(
+    alias='stg_logement_logements_sociaux_departement',
+    tags=['silver', 'logement_social']
+) }}
+
+with
+
+-- Historical indicators pivoted long with multiple years
+
+-- nb_ls
+nb_ls as (
+  {{ build_historical_indicator_cte(
+      ref('logement_logements_sociaux_departement'),
+      ['dep', 'libdep'],
+      'nb_ls',
+      'nb_ls',
+      exclusion_prefixes=[]
+  ) }}
+),
+
+-- tx_vac
+tx_vac as (
+  {{ build_historical_indicator_cte(
+      ref('logement_logements_sociaux_departement'),
+      ['dep', 'libdep'],
+      'tx_vac',
+      'tx_vac',
+      exclusion_prefixes=['tx_vac_3']
+  ) }}
+),
+
+-- tx_vac3
+tx_vac3 as (
+  {{ build_historical_indicator_cte(
+      ref('logement_logements_sociaux_departement'),
+      ['dep', 'libdep'],
+      'tx_vac3',
+      'tx_vac_3',
+      exclusion_prefixes=[]
+  ) }}
+),
+
+-- tx_mob
+tx_mob as (
+  {{ build_historical_indicator_cte(
+      ref('logement_logements_sociaux_departement'),
+      ['dep', 'libdep'],
+      'tx_mob',
+      'tx_mob',
+      exclusion_prefixes=[]
+  ) }}
+),
+
+-- Static indicators only present for next_year (e.g. nb_loues, nb_vacants, etc.)
+static_indicators as (
+  {{ build_static_indicator_cte(
+      ref('logement_logements_sociaux_departement'),
+      ['dep', 'libdep'],
+      ['nb_loues', 'nb_vacants', 'nb_vides', 'nb_asso', 'nb_occup_finan', 'nb_occup_temp', 'nb_ls_en_qpv']
+  ) }}
+),
+
+-- Densite block with fixed year (2021, from last census as of 01/01/2025)
+densite_block as (
+  select
+    dep as codgeo,
+    libdep,
+    2021 as year,
+    densite
+  from {{ ref('logement_logements_sociaux_departement') }}
+),
+
+-- Join all historical indicators + static indicators on geo + year
+joined_indicators as (
+  select
+    l.dep as codgeo,
+    l.libdep,
+    l.year,
+    l.indicateur as nb_ls,
+    tv.indicateur as tx_vac,
+    tv3.indicateur as tx_vac3,
+    tm.indicateur as tx_mob,
+    s.nb_loues,
+    s.nb_vacants,
+    s.nb_vides,
+    s.nb_asso,
+    s.nb_occup_finan,
+    s.nb_occup_temp,
+    s.nb_ls_en_qpv
+  from nb_ls l
+  left join tx_vac tv
+    on l.dep = tv.dep and l.year = tv.year
+  left join tx_vac3 tv3
+    on l.dep = tv3.dep and l.year = tv3.year
+  left join tx_mob tm
+    on l.dep = tm.dep and l.year = tm.year
+  left join static_indicators s
+    on l.dep = s.dep and l.year = s.year
+),
+
+-- Add densite and type_geo
+final as (
+  select
+    j.codgeo,
+    j.libdep,
+    j.year,
+    'departement' as type_geo,
+    j.nb_ls,
+    j.tx_vac,
+    j.tx_vac3,
+    j.tx_mob,
+    j.nb_loues,
+    j.nb_vacants,
+    j.nb_vides,
+    j.nb_asso,
+    j.nb_occup_finan,
+    j.nb_occup_temp,
+    j.nb_ls_en_qpv,
+    d.densite
+  from joined_indicators j
+  left join densite_block d
+    on j.codgeo = d.codgeo and j.year = d.year
+)
+
+select * from final
