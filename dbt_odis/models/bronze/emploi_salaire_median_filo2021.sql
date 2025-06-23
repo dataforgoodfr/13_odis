b1ea4fb6f92c8893148a9020b56696595c3b455e
ronan <syronanm@gmail.com>
2025-06-23 16:27:14 +0200

    add emploi salaire median db

--- dbt_odis/models/bronze/emploi_salaire_median_filo2021.sql
+++ dbt_odis/models/bronze/emploi_salaire_median_filo2021.sql
@@ -4,9 +4,8 @@
     )
 }}
 
-
 select 
-    "CODGEO" as "codgeo",
-    2021 as YEAR,
+    "CODGEO" as codgeo,
+    2021 as year,
     "Q221" as Mediane_Annuelle
 from {{ source('bronze', 'emploi_salaire_median_filo2021_disp_com') }}
