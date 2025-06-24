084a998fc6332d0b69c90bf0eddd45c23dbaf971
Victorien-M <maillet.victorien@gmail.com>
2025-06-24 15:40:36 +0200

    population_csp_bronze

--- dbt_odis/models/bronze/population_categorie_socio_pro.sql
+++ dbt_odis/models/bronze/population_categorie_socio_pro.sql
@@ -6,13 +6,14 @@
 
 with departments as 
 (
-    select 
+    select   
         (data::jsonb)->'dimensions'->>'AGE'::text as dimensions_AGE,        
         (data::jsonb)->'dimensions'->>'GEO'::text as dimensions_GEO,
         (data::jsonb)->'dimensions'->>'PCS'::text as dimensions_PCS,
         (data::jsonb)->'dimensions'->>'SEX'::text as dimensions_SEX,
         (data::jsonb)->'dimensions'->>'RP_MEASURE'::text as dimensions_RP_MEASURE,
-        (data::jsonb)->'dimensions'->>'TIME_PERIOD'::text as dimensions_TIME_PERIOD 
+        (data::jsonb)->'dimensions'->>'TIME_PERIOD'::text as dimensions_TIME_PERIOD,
+        (data::jsonb)->'measures'->'OBS_VALUE_NIVEAU'->>'value'::text as measures_OBS_VALUE_NIVEAU_value   
 
     from {{ source('bronze', 'population_categorie_socio_pro') }}  
     
--- dbt_odis/models/silver/silver_population_categorie_socio_pro.sql
+++ dbt_odis/models/silver/silver_population_categorie_socio_pro.sql
@@ -2,3 +2,9 @@
     alias='silver_population_categorie_socio_pro',
     tags=['silver', 'population_categorie_socio_pro']
 ) }}
+
+
+with population_csp as(
+    select * 
+    from {{ ref('population_categorie_socio_pro') }}  
+)    
