2957f39928451e83634b4ffa18547a2e43de40e4
Thomas Denoirjean <128964980+ThomasDenoirjean@users.noreply.github.com>
2025-06-24 12:23:08 +0000

    creating bronze view for population_repartition_sex and sex_age

---
+++ dbt_odis/models/bronze/population_repartition_sexe_age.sql
@@ -0,0 +1,21 @@
+{{ config(
+    tags = ['bronze', 'population'],
+    alias = 'vw_population_repartition_sexe_age'
+    )
+}}
+
+with repartition_sexe_age as (
+    select
+        id as id,
+        (data::jsonb)->'dimensions'->>'AGE'::text as dimensions_AGE, 
+        (data::jsonb)->'dimensions'->>'GEO'::text as dimensions_GEO,
+        (data::jsonb)->'dimensions'->>'SEX'::text as dimensions_SEX,
+        (data::jsonb)->'dimensions'->>'RP_MEASURE'::text as dimensions_RP_MEASURE,
+        (data::jsonb)->'dimensions'->>'TIME_PERIOD'::text as dimensions_TIME_PERIOD,
+        (data::jsonb)->'measures'->'OBS_VALUE_NIVEAU'->>'value'::text as measures_OBS_VALUE_NIVEAU_value,
+        created_at as created_at
+
+    from {{ source('bronze', 'population_repartition_sexe_age') }}  
+)
+
+select * from repartition_sexe_age
