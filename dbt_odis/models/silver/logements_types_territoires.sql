008884a6f21dc75db2e8a476516a2c23c13bc8fe
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2025-06-25 17:30:33 +0200

    reconstruction du silver logements classiques, évacuation des anciens models appt et maisons_rp

--- dbt_odis/models/silver/logements_types_territoires.sql
+++ dbt_odis/models/silver/logements_types_territoires.sql
@@ -4,83 +4,108 @@
     )
 }}
 
-with apt_ as 
-(
+with appartements as (
     select 
-        distinct measures_OBS_VALUE_NIVEAU_value,
-        dimensions_GEO
-    from {{ ref('logements_appartement') }}  
+        dimensions_GEO,
+        dimensions_TIME_PERIOD,
+        max(case when dimensions_ocs = '_T' then measures_OBS_VALUE_NIVEAU_value end) as total_appartements,
+        max(case when dimensions_ocs = 'DW_MAIN' then measures_OBS_VALUE_NIVEAU_value end) as rp_appartements,
+        max(case when dimensions_ocs = 'DW_SEC_DW_OCC' then measures_OBS_VALUE_NIVEAU_value end) as rsecocc_appartements,
+        max(case when dimensions_ocs = 'DW_VAC' then measures_OBS_VALUE_NIVEAU_value end) as lvac_appartements
+    from {{ ref('logements_appartement') }}
+    group by dimensions_GEO, dimensions_TIME_PERIOD  
 ),
 
-apt_rp as 
+maisons as 
 (
     select 
-        distinct measures_OBS_VALUE_NIVEAU_value,
-        dimensions_GEO
-    from {{ ref('logements_appartement_et_residences_principales') }} 
-),
-
-maison_ as 
-(
-    select 
-        distinct measures_OBS_VALUE_NIVEAU_value,
-        dimensions_GEO
+        dimensions_GEO,
+        dimensions_TIME_PERIOD,
+        max(case when dimensions_ocs = '_T' then measures_OBS_VALUE_NIVEAU_value end) as total_maisons,
+        max(case when dimensions_ocs = 'DW_MAIN' then measures_OBS_VALUE_NIVEAU_value end) as rp_maisons,
+        max(case when dimensions_ocs = 'DW_SEC_DW_OCC' then measures_OBS_VALUE_NIVEAU_value end) as rsecocc_maisons,
+        max(case when dimensions_ocs = 'DW_VAC' then measures_OBS_VALUE_NIVEAU_value end) as lvac_maisons
     from {{ ref('logements_maison') }} 
-),
-
-maison_rp as 
-(
-    select 
-        distinct measures_OBS_VALUE_NIVEAU_value,
-        dimensions_GEO
-    from {{ ref('logements_maison_et_residences_principales') }} 
+    group by dimensions_GEO, dimensions_TIME_PERIOD
 ),
 
 pieces as 
 (
     select 
-        distinct measures_OBS_VALUE_NIVEAU_value,
-        dimensions_GEO
-
+        dimensions_GEO,
+        dimensions_TIME_PERIOD,
+        dimensions_RP_MEASURE,
+        max(case when dimensions_tdw = '_T' then measures_OBS_VALUE_NIVEAU_value end) as total_rp_pieces,
+        max(case when dimensions_tdw = '1' then measures_OBS_VALUE_NIVEAU_value end) as maisons_rp_pieces,
+        max(case when dimensions_tdw = '2' then measures_OBS_VALUE_NIVEAU_value end) as appartements_rp_pieces,
+        max(case when dimensions_tdw = '3T6' then measures_OBS_VALUE_NIVEAU_value end) as autres_rp_pieces
     from {{ ref('logements_pieces') }} 
+    group by dimensions_GEO, dimensions_TIME_PERIOD, dimensions_RP_MEASURE
 ),
 
-total as 
+total_logements as 
 (
     select 
-        distinct *
+        dimensions_GEO,
+        dimensions_TIME_PERIOD,
+        dimensions_RP_MEASURE,
+        max(case when dimensions_ocs = '_T' and dimensions_tdw = '_T' then measures_OBS_VALUE_NIVEAU_value end) as total_tous_logements,
+        max(case when dimensions_ocs = 'DW_MAIN' and dimensions_tdw = '_T' then measures_OBS_VALUE_NIVEAU_value end) as rp_tous_logements,
+        max(case when dimensions_ocs = 'DW_SEC_DW_OCC' and dimensions_tdw = '_T' then measures_OBS_VALUE_NIVEAU_value end) as rsecocc_tous_logements,
+        max(case when dimensions_ocs = 'DW_VAC' and dimensions_tdw = '_T' then measures_OBS_VALUE_NIVEAU_value end) as lvac_tous_logements
     from {{ ref('logements_total') }} 
+    group by dimensions_GEO, dimensions_TIME_PERIOD, dimensions_RP_MEASURE
 ),
 
 joined as (
     select 
-        tot.id,
         tot.dimensions_GEO as geo,
         tot.dimensions_TIME_PERIOD as time_period,
-        maison.measures_OBS_VALUE_NIVEAU_value as maisons,
-        maison_rp.measures_OBS_VALUE_NIVEAU_value as maisons_rp,
-        apt.measures_OBS_VALUE_NIVEAU_value as appartements,
-        apt_rp.measures_OBS_VALUE_NIVEAU_value as appartements_rp,
-        pieces.measures_OBS_VALUE_NIVEAU_value as pieces,
-        tot.measures_OBS_VALUE_NIVEAU_value as total_logements
-    from total tot
-        left join maison_ maison on maison.dimensions_GEO = tot.dimensions_GEO
-        left join maison_rp maison_rp on maison_rp.dimensions_GEO = tot.dimensions_GEO
-        left join apt_ apt on apt.dimensions_GEO = tot.dimensions_GEO
-        left join apt_rp apt_rp on apt_rp.dimensions_GEO = tot.dimensions_GEO
-        left join pieces pieces on pieces.dimensions_GEO = tot.dimensions_GEO
+        tot.dimensions_RP_MEASURE as habitations,
+        tot.total_tous_logements,
+        tot.rp_tous_logements,
+        tot.rsecocc_tous_logements,
+        tot.lvac_tous_logements,
+        maisons.total_maisons,
+        maisons.rp_maisons,
+        maisons.rsecocc_maisons,
+        maisons.lvac_maisons,
+        appts.total_appartements,
+        appts.rp_appartements,
+        appts.rsecocc_appartements,
+        appts.lvac_appartements,
+        pieces.dimensions_RP_MEASURE as pieces_habitations,
+        pieces.total_rp_pieces,
+        pieces.maisons_rp_pieces,
+        pieces.appartements_rp_pieces,
+        pieces.autres_rp_pieces
+    from total_logements tot
+        left join maisons maisons on maisons.dimensions_GEO = tot.dimensions_GEO and maisons.dimensions_TIME_PERIOD = tot.dimensions_TIME_PERIOD
+        left join appartements appts on appts.dimensions_GEO = tot.dimensions_GEO and appts.dimensions_TIME_PERIOD = tot.dimensions_TIME_PERIOD
+        left join pieces pieces on pieces.dimensions_GEO = tot.dimensions_GEO and pieces.dimensions_TIME_PERIOD = tot.dimensions_TIME_PERIOD
 )
 
 select 
-    id,
     geo,
     time_period,
-    cast(maisons as float) as maisons,
-    cast(maisons_rp as float) as maisons_rp,
-    cast(appartements as float) as appartements,
-    cast(appartements_rp as float) as appartements_rp,
-    cast(pieces as float) as pieces,
-    cast(total_logements as float) as total_logements,
+    habitations,
+    cast(total_tous_logements as float) as total_tous_logements,
+    cast(rp_tous_logements as float) as rp_tous_logements,
+    cast(rsecocc_tous_logements as float) as rsecocc_tous_logements,
+    cast(lvac_tous_logements as float) as lvac_tous_logements,
+    cast(total_maisons as float) as total_maisons,
+    cast(rp_maisons as float) as rp_maisons,
+    cast(rsecocc_maisons as float) as rsecocc_maisons,
+    cast(lvac_maisons as float) as lvac_maisons,
+    cast(total_appartements as float) as total_appartements,
+    cast(rp_appartements as float) as rp_appartements,
+    cast(rsecocc_appartements as float) as rsecocc_appartements,
+    cast(lvac_appartements as float) as lvac_appartements,
+    pieces_habitations,
+    cast(total_rp_pieces as float) as total_rp_pieces,
+    cast(maisons_rp_pieces as float) as maisons_rp_pieces,
+    cast(appartements_rp_pieces as float) as appartements_rp_pieces,
+    cast(autres_rp_pieces as float) as autres_rp_pieces,
     case
         when geo like '%COM%' then 'commune'
         when geo like '%DEP%' then 'departement'
