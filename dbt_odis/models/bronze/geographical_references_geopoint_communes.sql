ff5550d0e9f006db2cf69e8240c60454a09d02b8
Thomas Denoirjean <128964980+ThomasDenoirjean@users.noreply.github.com>
2025-06-25 14:00:28 +0000

    removed geoshape from sql request

--- dbt_odis/models/bronze/geographical_references_geopoint_communes.sql
+++ dbt_odis/models/bronze/geographical_references_geopoint_communes.sql
@@ -10,10 +10,8 @@ with geopoint_communes as
     select 
         "a" as id,
         "GeoPoint" as GeoPoint,
-        "GeoShape"::json -> 'coordinates' as GeoShape,
         "Annee" as Annee,
         "Code_Officiel_Commune" as Code_Officiel_Commune,
-        "Code_Officiel_Courant_Commune" as Code_Officiel_Courant_Commune,
         "latitude" as Latitude,
         "longitude" as Longitude
     from {{ source('bronze', 'corresp_geo_point_codes_communes') }} 
