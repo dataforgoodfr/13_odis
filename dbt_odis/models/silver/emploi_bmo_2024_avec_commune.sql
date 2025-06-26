a217ee026f814aaba92e08d7a9b0ca9a7ffa0999
Cy <102252871+cysearch@users.noreply.github.com>
2025-06-26 10:58:28 +0200

    somme des met, smet, xmet par département Puis jointure sur la table des communes pour avoir l'info en une ligne par commune.

--- dbt_odis/models/silver/emploi_bmo_2024_avec_commune.sql
+++ dbt_odis/models/silver/emploi_bmo_2024_avec_commune.sql
@@ -1,57 +1,99 @@
 {{ config(
     tags = ['silver', 'emploi'],
-    alias = 'emploi_bmo_2024_avec_commune'
+    alias = 'emploi_bmo_2024_avec_communes'
 ) }}
 
-with bassin_commune as (
-    select
-        code_commune_insee,
-        "code_bassin_BMO"
-    from {{ ref('bassin_emploi') }}
-),
-fap as (
-    select
-        code_fap_228,
-        intitule_fap_22
-    from {{ ref('dares_nomenclature_fap2021') }}
-),
-bmo as (
-    select *
-    from {{ source('bronze', 'vw_emploi_bmo_2024') }}
-),
-
-jointure as (
-    select
-        bmo.*,
-        bc.code_commune_insee,
-        fap.intitule_fap_22
-    from bmo
-    left join bassin_commune bc
-        on bc."code_bassin_BMO" = bmo."BE24"
-    left join fap fap
-        on fap.code_fap_228 = bmo."Code métier BMO"
-)
-
-select
-    -- on garde toutes les colonnes sauf 'met' que l'on traite :
-    annee,
-    "Code métier BMO",
-    "Nom métier BMO",
-    "Famille_met",
-    "Lbl_fam_met",
-    "REG",
-    "NOM_REG",
-    "Dept",
-    "NomDept",
-    "BE24",
-    "NOMBE24",
-    case
-        when met = '*' then 0
-        else met::integer
-    end as met,
-    xmet,
-    smet,
-    code_commune_insee,
-    intitule_fap_22
-
-from jointure
+with
+    bmo as (
+        select
+            "annee",
+            "Code métier BMO",
+            "Nom métier BMO",
+            "Famille_met",
+            "Lbl_fam_met",
+            "REG",
+            "NOM_REG",
+            "Dept",
+            "NomDept",
+            "BE24",
+            "NOMBE24",
+            case
+                when met = '*' then 1
+                else met::integer
+            end as "met",
+            case
+                when xmet = '*' then 1
+                else xmet::integer
+            end as "xmet",
+            case
+                when smet = '*' then 1
+                else smet::integer
+            end as "smet"
+        from {{ source('bronze', 'vw_emploi_bmo_2024') }}
+    ),
+    bmo_sum_dept as (
+        select
+            "annee",
+            "Code métier BMO",
+            "Nom métier BMO",
+            "Famille_met",
+            "Lbl_fam_met",
+            "REG",
+            "NOM_REG",
+            "BE24",
+            "NOMBE24",
+            sum(met) as "met",
+            sum(xmet) as "xmet",
+            sum(smet) as "smet"
+        from bmo
+        group by
+            "annee",
+            "Code métier BMO",
+            "Nom métier BMO",
+            "Famille_met",
+            "Lbl_fam_met",
+            "REG",
+            "NOM_REG",
+            "BE24",
+            "NOMBE24"
+    ),
+    departements as (
+        select distinct
+            "Dept",
+            "NomDept"
+        from bmo
+    ),
+    bassins_communes as (
+        select
+            "dep",
+            "code_commune_insee",
+            "code_bassin_BMO"
+        from {{ ref('bassin_emploi') }}
+    ),
+    bmo_be_sum_communes as (
+        select
+            bmo_sum_dept."annee",
+            bmo_sum_dept."Code métier BMO",
+            bmo_sum_dept."Nom métier BMO",
+            bmo_sum_dept."Famille_met",
+            bmo_sum_dept."Lbl_fam_met",
+            bmo_sum_dept."REG",
+            bmo_sum_dept."NOM_REG",
+            departements."Dept",
+            departements."NomDept",
+            bassins_communes."code_commune_insee",
+            bmo_sum_dept."BE24",
+            bmo_sum_dept."NOMBE24",
+            bmo_sum_dept."met",
+            bmo_sum_dept."xmet",
+            bmo_sum_dept."smet"
+        from  bassins_communes
+        left join bmo_sum_dept
+        on bassins_communes."code_bassin_BMO" = bmo_sum_dept."BE24"
+        left join departements
+        on bassins_communes."dep" = departements."Dept"
+    )
+select * from bmo_be_sum_communes
+where "Code métier BMO" = 'A0X40'
+and ("BE24" = '1119'
+or "BE24" = '1127')
