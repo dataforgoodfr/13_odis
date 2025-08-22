132574d63f7007c6d20ed654e75fbfaa469be27d
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2025-08-22 15:27:50 +0200

    Transfo services_services bronze into gold + docu, export ok

--- dbt_odis/models/bronze/services_services.sql
+++ dbt_odis/models/bronze/services_services.sql
@@ -9,13 +9,13 @@ with services as
 (
     select 
         id, 
-        (data::jsonb)-> 'measures' -> 'OBS_VALUE_NIVEAU' ->> 'value' as value, 
         (data::jsonb)-> 'dimensions' ->> 'GEO' as GEO, 
         (data::jsonb)-> 'dimensions' ->> 'BPE_MEASURE' as BPE_MEASURE,
         (data::jsonb)-> 'dimensions' ->> 'TIME_PERIOD' as TIME_PERIOD,
         (data::jsonb)-> 'dimensions' ->> 'FACILITY_DOM' as FACILITY_DOM,
         (data::jsonb)-> 'dimensions' ->> 'FACILITY_SDOM' as FACILITY_SDOM,
         (data::jsonb)-> 'dimensions' ->> 'FACILITY_TYPE' as FACILITY_TYPE,
+        (data::jsonb)-> 'measures' -> 'OBS_VALUE_NIVEAU' ->> 'value' as value, 
         created_at
     from {{ source('bronze', 'services_services') }} 
 )
