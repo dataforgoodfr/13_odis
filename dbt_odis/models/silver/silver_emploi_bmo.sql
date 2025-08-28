d593d58abb28d8ce4a55d7859246bbbb695a77b7
Jean-Baptiste-N <jeanbaptiste.nez@gmail.com>
2025-08-28 17:18:57 +0200

    construction nouveau silver + corrections codes INSEE + test table ok

--- dbt_odis/models/silver/silver_emploi_bmo.sql
+++ dbt_odis/models/silver/silver_emploi_bmo.sql
@@ -20,20 +20,21 @@ with codes as (
         "Code_FAP22",
         "Intitulé_FAP22"
 
-)
+),
+bmo as (
     select
-        b."annee",
+        extract(year from cast(b."annee" || '-01-01' as date)) as "YEAR",
         b."Code métier BMO",
         b."Nom métier BMO",
         b."Famille_met",
         b."Lbl_fam_met",
         f."Code_FAP22",
         f."Intitulé_FAP22",
-        b."REG" ::text,
+        lpad(b."REG" ::text, 2, '0') as "REG",
         b."NOM_REG",
         b."Dept",
         b."NomDept",
-        b."BE24" ::text,
+        lpad(b."BE24" ::text, 4, '0') as "BE24",
         b."NOMBE24",
         case
             when b."met" = '*' then 1
@@ -50,67 +51,116 @@ with codes as (
     from {{ ref('emploi_bmo_2024') }} b
         left join codes f
         on b."Code métier BMO" = f."Code_FAP228"
-{# ),
-    bmo_sum_dept as (
-        select
-            "annee",
-            "Code métier BMO",
-            "Nom métier BMO",
-            "Famille_met",
-            "Lbl_fam_met",
-            "REG",
-            "NOM_REG",
-            "BE24",
-            "NOMBE24",
-            SUM(met) as "met",
-            SUM(xmet) as "xmet",
-            SUM(smet) as "smet"
-        from bmo
-        GROUP BY
-            "annee",
-            "Code métier BMO",
-            "Nom métier BMO",
-            "Famille_met",
-            "Lbl_fam_met",
-            "REG",
-            "NOM_REG",
-            "BE24",
-            "NOMBE24"
-    ),
-    departements as (
-        select DISTINCT
-            "Dept",
-            "NomDept"
-        from bmo
-    ),
-    bassins_communes as (
-        select
-            LPAD("dep"::text, 2, '0') as "dep",
-            "code_commune_insee",
-            "code_bassin_BMO"
-        from {{ ref('bassin_emploi') }}
-    ),
-    bmo_be_sum_communes as (
-        select
-            bmo_sum_dept."annee",
-            bmo_sum_dept."Code métier BMO",
-            bmo_sum_dept."Nom métier BMO",
-            bmo_sum_dept."Famille_met",
-            bmo_sum_dept."Lbl_fam_met",
-            bmo_sum_dept."REG",
-            bmo_sum_dept."NOM_REG",
-            departements."Dept",
-            departements."NomDept",
-            bassins_communes."code_commune_insee",
-            bmo_sum_dept."BE24",
-            bmo_sum_dept."NOMBE24",
-            bmo_sum_dept."met",
-            bmo_sum_dept."xmet",
-            bmo_sum_dept."smet"
-        from bassins_communes
-        LEFT JOIN bmo_sum_dept
-            ON bassins_communes."code_bassin_BMO" = bmo_sum_dept."BE24"
-        LEFT JOIN departements
-            ON bassins_communes."dep" = departements."Dept"
-    )
-select * from bmo_be_sum_communes #}
+),
+
+communes_bassins as (
+    select
+        lpad(reg, 2, '0') as "reg",
+        lpad(dep, 2, '0') as "dep",
+        code_commune_insee,
+        lpad("code_bassin_BMO"::text, 4, '0') as "code_bassin_BMO",
+        "lib_bassin_BMO"
+    from {{ ref('bassin_emploi') }}
+),
+
+join_communes as (
+    select
+        bmo."YEAR",
+        bmo."Code métier BMO",
+        bmo."Nom métier BMO",
+        bmo."Famille_met",
+        bmo."Lbl_fam_met",
+        bmo."Code_FAP22",
+        bmo."Intitulé_FAP22",
+        bmo."REG",
+        bmo."NOM_REG",
+        bmo."Dept",
+        bmo."NomDept",
+        bmo."BE24",
+        bmo."NOMBE24",
+        bmo."met",
+        bmo."xmet",
+        bmo."smet",
+        cb."code_commune_insee" as code_geo,
+        'commune' as type_geo
+    from bmo
+    right join communes_bassins cb
+    on left(bmo."Dept"::text, 2) = cb."dep"
+    and bmo."BE24" = cb."code_bassin_BMO"
+    and bmo."REG" = cb."reg"
+
+),
+
+sum_dept as (
+    select
+        "YEAR",
+        "Code métier BMO",
+        "Nom métier BMO",
+        "Famille_met",
+        "Lbl_fam_met",
+        "Code_FAP22",
+        "Intitulé_FAP22",
+        "REG",
+        "NOM_REG",
+        "Dept",
+        "NomDept",
+        null as "BE24",
+        null as "NOMBE24",
+        sum(met) as "met",
+        sum(xmet) as "xmet",
+        sum(smet) as "smet",
+        "Dept" as code_geo,
+        'département' as type_geo
+    from bmo
+    group by
+        "YEAR",
+        "Code métier BMO",
+        "Nom métier BMO",
+        "Famille_met",
+        "Lbl_fam_met",
+        "Code_FAP22",
+        "Intitulé_FAP22",
+        "REG",
+        "NOM_REG",
+        "Dept",
+        "NomDept"
+),
+
+sum_reg as (
+    select
+        "YEAR",
+        "Code métier BMO",
+        "Nom métier BMO",
+        "Famille_met",
+        "Lbl_fam_met",
+        "Code_FAP22",
+        "Intitulé_FAP22",
+        "REG",
+        "NOM_REG",
+        null as "Dept",
+        null as "NomDept",
+        null as "BE24",
+        null as "NOMBE24",
+        sum(met) as "met",
+        sum(xmet) as "xmet",
+        sum(smet) as "smet",
+        concat('reg', "REG") as code_geo,
+        'région' as type_geo
+    from bmo
+    group by
+        "YEAR",
+        "Code métier BMO",
+        "Nom métier BMO",
+        "Famille_met",
+        "Lbl_fam_met",
+        "Code_FAP22",
+        "Intitulé_FAP22",
+        "REG",
+        "NOM_REG"
+)
+
+select * from join_communes
+union all
+select * from sum_dept
+union all
+select * from sum_reg
