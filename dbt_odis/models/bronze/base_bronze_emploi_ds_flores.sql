0bff923259aad3983769c632bc0e07647fe92bbd
Thomas Grammatico <thomas@grammatico.me>
2025-10-14 14:06:09 +0000

    Fixed Bronze layer for emploi_ds_flores.

--- dbt_odis/models/bronze/base_bronze_emploi_ds_flores.sql
+++ dbt_odis/models/bronze/base_bronze_emploi_ds_flores.sql
@@ -9,18 +9,14 @@ with ds_flores as
 (
     select 
         id,
-        {# {{ extract_json_keys_recursive(
-            table_ref = source('bronze', 'emploi_ds_flores'),
-            json_column = 'data'
-        ) }}, #}
-        (data::jsonb)->'dimensions'->>'GEO'::text as dimensions_GEO,
-        (data::jsonb)->'dimensions'->>'ECONOMIC_SPHERE'::text as dimentions_economic_sphere,
-        (data::jsonb)->'dimensions'->>'FLORES_MEASURE'::text as dimensions_flores_measure,
-        (data::jsonb)->'dimensions'->>'LEGAL_FORM_WITH_PUBLIC'::text as dimensions_legal_form,
-        (data::jsonb)->'dimensions'->>'TIME_PERIOD'::text as dimensions_TIME_PERIOD,
-        (data::jsonb)->'measures'->'OBS_VALUE_NIVEAU'->>'value'::text as measures_OBS_VALUE_NIVEAU_value,
+        (obs.value::jsonb)->'dimensions'->>'GEO'::text as dimensions_GEO,
+        (obs.value)->'dimensions'->>'ECONOMIC_SPHERE'::text as dimentions_economic_sphere,
+        (obs.value)->'dimensions'->>'FLORES_MEASURE'::text as dimensions_flores_measure,
+        (obs.value)->'dimensions'->>'LEGAL_FORM_WITH_PUBLIC'::text as dimensions_legal_form,
+        (obs.value)->'dimensions'->>'TIME_PERIOD'::text as dimensions_TIME_PERIOD,
+        (obs.value)->'measures'->'OBS_VALUE_NIVEAU'->>'value'::text as measures_OBS_VALUE_NIVEAU_value,
         created_at
-    from {{ source('bronze', 'emploi_ds_flores') }} 
+    from {{ source('bronze', 'emploi_ds_flores') }} , jsonb_array_elements((data::jsonb)->'observations') as obs
 )
 
 select * from ds_flores
