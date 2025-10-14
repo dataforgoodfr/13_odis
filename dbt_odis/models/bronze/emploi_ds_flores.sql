a629ca16ecf8d3258af2aec11a6ff206d999fac3
Thomas Grammatico <thomas@grammatico.me>
2025-10-14 15:28:40 +0000

    Casted columns to correct type.

--- dbt_odis/models/bronze/emploi_ds_flores.sql
+++ dbt_odis/models/bronze/emploi_ds_flores.sql
@@ -17,12 +17,22 @@ with ds_flores as
         (obs.value::jsonb)->'measures'->'OBS_VALUE_NIVEAU'->>'value'::text as measures_OBS_VALUE_NIVEAU_value,
         created_at
     from {{ source('bronze', 'emploi_ds_flores') }} , jsonb_array_elements((data::jsonb)->'observations') as obs
+),
+
+ds_flores_wunique_id as(
+    select
+        {{ dbt_utils.generate_surrogate_key(['dimensions_GEO', 
+                                        'dimensions_legal_form', 
+                                        'dimensions_TIME_PERIOD',
+                                        'dimensions_economic_sphere',
+                                        'dimensions_flores_measure']) }} as id,
+        dimensions_GEO, 
+        dimensions_economic_sphere, 
+        dimensions_flores_measure,
+        dimensions_legal_form,
+        cast(dimensions_TIME_PERIOD as int),
+        cast(measures_OBS_VALUE_NIVEAU_value as float)
+    from ds_flores
 )
 
-  select md5(ROW(
-      dimensions_GEO,
-      dimensions_economic_sphere,
-      dimensions_flores_measure,
-      dimensions_legal_form,
-      dimensions_TIME_PERIOD
-  )::TEXT) as id, * from ds_flores
+  select * from ds_flores_wunique_id
