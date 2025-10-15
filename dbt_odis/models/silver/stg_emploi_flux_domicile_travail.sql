a0cc60582d0ca534be41bac7f651d4f4e98e5311
Thomas Grammatico <thomas@grammatico.me>
2025-10-15 13:57:48 +0000

    Added gold layer communes_travail. Fixed silver layer to remove cases where flux was from and to the same city.

--- dbt_odis/models/silver/stg_emploi_flux_domicile_travail.sql
+++ dbt_odis/models/silver/stg_emploi_flux_domicile_travail.sql
@@ -3,7 +3,7 @@
     alias='vw_emploi_flux_domicile_travail_stg',
     materialized='view'
 ) }}
-
+with cleaned_arr as(
 select
     index,
     case when "LIBGEO" like 'Paris%'
@@ -16,6 +16,14 @@ select
     end as codgeo,
     2022 as "YEAR",
     case when "L_DCLT" like 'Paris%'
+            then '75056'
+        when "L_DCLT" like 'Marseille%'
+            then '13055'
+        when "L_DCLT" like 'Lyon%'
+            then '69123'
+        else "DCLT"
+    end as "DCLT",
+    case when "L_DCLT" like 'Paris%'
             then 'Paris'
         when "L_DCLT" like 'Marseille%'
             then 'Marseille'
@@ -25,3 +33,12 @@ select
     end as "Libellé - lieu de travail",
     "NBFLUX_C22_ACTOCC15P" as "Flux d'actifs de 15 ans ou plus ayant un emploi"
 from {{ ref('emploi_flux_domicile_travail') }}
+)
+
+select  index,
+        codgeo,
+        "YEAR",
+        "Libellé - lieu de travail",
+        "Flux d'actifs de 15 ans ou plus ayant un emploi"
+from cleaned_arr
+where (codgeo != "DCLT") --remove case where flux is from and to the same city
