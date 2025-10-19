bc6bf40e3b7c83f70cb9b86cfae6b782c53ccdd8
HAMIDI Amin <hamidi.amin@hotmail.fr>
2025-10-19 18:43:43 +0000

    add the three gold models with the same columns and naming

---
+++ dbt_odis/models/gold/gold_presentation_elu_reg.sql
@@ -0,0 +1,26 @@
+{{ config(
+    tags = ['silver', 'presentation','elu'],
+    )
+}}
+
+with renommage_et_ajout_nuance as (
+    select
+        presentation_elus_communes.reg_code as "Code de la région",
+        presentation_elus_communes.reg_name as "Nom de la région",
+        presentation_elus_communes.prenom_de_l_elu as "Prénom de l’élu",
+        presentation_elus_communes.nom_de_l_elu as "Nom de l’élu",
+        presentation_elus_communes.libelle_de_la_fonction as "Nom de la fonction",
+        nuance_politique.libelle_nuance as "Libellé de la nuance du mandat",
+        presentation_elus_communes.type_de_la_fonction as "Type de la fonction",
+        presentation_elus_communes.date_de_debut_du_mandat as "Date de début du mandat",
+        presentation_elus_communes.date_de_debut_de_la_fonction as "Date de début de la fonction",
+        presentation_elus_communes.code_sexe as "Genre de l’élu"
+    from {{ ref ("silver_presentation_elus_communes") }} as presentation_elus_communes
+    left join {{ ref ("silver_presentation_elus_dim_nuance_politique") }} as nuance_politique
+        on presentation_elus_communes.nom_de_l_elu = nuance_politique.nom
+        and presentation_elus_communes.prenom_de_l_elu = nuance_politique.prenom
+        --and presentation_elus_communes.com_code = nuance_politique.code_officiel_commune
+    where type_de_la_fonction = 'Régional'
+)
+
+select * from renommage_et_ajout_nuance
