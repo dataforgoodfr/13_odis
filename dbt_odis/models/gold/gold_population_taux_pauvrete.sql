1e3707a76a98cd47c99955b10711573db1e1de61
Thomas Grammatico <thomas@grammatico.me>
2025-11-08 17:10:55 +0000

    Removed admin_level from gold_population_taux_pauvrete. We differentiate region and departement with the reg prefix.

--- dbt_odis/models/gold/gold_population_taux_pauvrete.sql
+++ dbt_odis/models/gold/gold_population_taux_pauvrete.sql
@@ -5,10 +5,14 @@
 }}
 
 select
-    "CODGEO" as codgeo,
+    case
+        when admin_level = 'commune' then "CODGEO"
+        when admin_level = 'departement' then "CODGEO"
+        when admin_level = 'region' then concat('reg', "CODGEO")
+        else "CODGEO"
+    end as codgeo,    
     "YEAR",
     "TP40",
     "TP50",
-    "TP60",
-    admin_level
+    "TP60"
 from {{ref("stg_population_taux_pauvrete")}}
