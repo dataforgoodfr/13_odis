107a7a3d2364c42c352f3919fc2d648e097cf85c
Thomas Grammatico <thomas@grammatico.me>
2025-11-19 13:14:02 +0000

    Fixed naming convention so we drop 'page' everywhere. Fixed bronze presentation_population_communes in order to access data in observations array.

--- dbt_odis/models/bronze/presentation_population_communes.sql
+++ dbt_odis/models/bronze/presentation_population_communes.sql
@@ -1,22 +1,20 @@
 {{ config(
     tags = ['bronze', 'presentation'],
     alias = 'vw_presentation_population_communes'
-    )
-}}
-
+) }}
 
 with population_communes as 
 (
     select 
-        id, 
-        (data::jsonb)->'measures' -> 'OBS_VALUE_NIVEAU' ->> 'value' as value, 
-        (data::jsonb)->'dimensions' ->> 'AGE' as AGE, 
-        (data::jsonb)->'dimensions' ->> 'GEO' as GEO,
-        (data::jsonb)->'dimensions' ->> 'SEX' as SEX,
-        (data::jsonb)->'dimensions' ->> 'RP_MEASURE' as RP_MEASURE,
-        (data::jsonb)->'dimensions' ->> 'TIME_PERIOD' as TIME_PERIOD,
+        obs->'measures'->'OBS_VALUE_NIVEAU'->>'value' as value, 
+        obs->'dimensions'->>'AGE' as AGE, 
+        obs->'dimensions'->>'GEO' as GEO,
+        obs->'dimensions'->>'SEX' as SEX,
+        obs->'dimensions'->>'RP_MEASURE' as RP_MEASURE,
+        obs->'dimensions'->>'TIME_PERIOD' as TIME_PERIOD,
         created_at
-    from {{ source('bronze', 'presentation_population_communes') }} 
+    from {{ source('bronze', 'presentation_population_communes') }},
+    jsonb_array_elements(data->'observations') as obs
 )
 
 select * from population_communes
