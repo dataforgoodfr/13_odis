523a49cc7522cd63f0e15e49f2b7eefa90227bf7
HAMIDI Amin <hamidi.amin@hotmail.fr>
2025-11-28 00:20:19 +0000

    fix: no need to change dep type in elu_commune since it's already in the right one

--- dbt_odis/models/bronze/presentation_elus_communes.sql
+++ dbt_odis/models/bronze/presentation_elus_communes.sql
@@ -6,7 +6,6 @@
 
 select
   {{ dbt_utils.star(from=source('bronze', 'presentation_elus_communes'), 
-    except=["dep_code","reg_code"]) }},
-  lpad(trunc(reg_code::numeric)::text, 2, '0')  as reg_code,
-  lpad(trunc(dep_code::numeric)::text, 2, '0') as dep_code
+    except=["reg_code"]) }},
+  lpad(trunc(reg_code::numeric)::text, 2, '0')  as reg_code
 from {{ source('bronze', 'presentation_elus_communes') }}
