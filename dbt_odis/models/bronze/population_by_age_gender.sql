1aee3bb878e3362b8fd2c04a805b9569c9fddf5d
juliette <wipgarden57@protonmail.com>
2025-12-09 14:28:31 +0100

    dbt doc

--- dbt_odis/models/bronze/population_by_age_gender.sql
+++ dbt_odis/models/bronze/population_by_age_gender.sql
@@ -11,8 +11,8 @@ with people as
         (data::jsonb)->'dimensions'->>'GEO'::text as geo,
         (data::jsonb)->'dimensions'->>'SEX'::text as sex,
         (data::jsonb)->'dimensions'->>'AGE'::text as age,
-        (data::jsonb)->'dimensions'->>'RP_MEASURE'::text as rp_measure,
         (data::jsonb)->'dimensions'->>'TIME_PERIOD'::text as time_period,
+        (data::jsonb)->'dimensions'->>'RP_MEASURE'::text as rp_measure,
         (data::jsonb)->'measures'->'OBS_VALUE_NIVEAU'->>'value'::text as measure_value,
         created_at
     from {{ source('bronze', 'population_by_age_gender') }} 
