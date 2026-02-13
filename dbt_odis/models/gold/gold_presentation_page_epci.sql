08fd3fa8b920770f2788f990fd4a706bf502dc0e
Thomas Grammatico <thomas@grammatico.me>
2026-02-13 12:35:41 +0000

    Fixed presentation column names.

--- dbt_odis/models/gold/gold_presentation_page_epci.sql
+++ dbt_odis/models/gold/gold_presentation_page_epci.sql
@@ -4,5 +4,11 @@
 ) }}
 
 SELECT
-    *
+    codgeo,
+    raison_sociale,
+    nb_membres,
+    total_ptot_communes as total_pop_tot,
+    total_pmun_communes as total_pop_mun,
+    ptot_commune as ptot_2023,
+    pmun_commune as pmun_2023
 FROM {{ ref('silver_presentation_page_epci') }}
